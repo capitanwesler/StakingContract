edd3bc095999fd6abfb45b451fbe95237843e9d1
capitanwesler <guillermo.campanudo@hotmail.com>
2021-06-22 16:45:04 +0100

    Deleting unused swapper

--- contracts/StakingContract.sol
+++ contracts/StakingContract.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/utils/Context.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./interfaces/IWeth.sol";
 import "hardhat/console.sol";
 
@@ -17,6 +18,7 @@ import "hardhat/console.sol";
 
 contract StakingContract is Initializable, Context {
   using SafeMath for uint256;
+  using SafeERC20 for IERC20;
   address constant FactoryUniswap = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
   address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
   address public owner;
@@ -40,6 +42,7 @@ contract StakingContract is Initializable, Context {
   **/
   function getPairAndBalance(address _tokenA, address _tokenB) public payable {
     console.log("Balance of User in ETH: >> %s", _msgSender().balance);
+    IERC20(_tokenB).safeTransferFrom(_msgSender(), address(this), 300 * 1e18);
     IWeth(WETH).deposit{value: msg.value}();
     console.log("Balance of User in WETH: >> %s", IWeth(WETH).balanceOf(address(this)));
     IWeth(WETH).transfer(_getAddressPair(_tokenA, _tokenB), msg.value);
--- test/StakingContract.test.js
+++ test/StakingContract.test.js
@@ -1,47 +1,5 @@
 const { ethers, upgrades } = require('hardhat');
 
-const LINK = '0x514910771AF9Ca656af840dff83E8264EcF986CA';
-const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
-const USDT = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
-const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
-
-describe('Swapper: ETH for Tokens', () => {
-  let swapper;
-  let DAItoken;
-  let LINKtoken;
-  let owner;
-
-  before(async () => {
-    // - Getting the factories for the contracts:
-    const Swapper = await ethers.getContractFactory('Swapper');
-
-    [owner] = await ethers.getSigners();
-
-    swapper = await Swapper.deploy(owner.address);
-    await swapper.deployed();
-
-    // DAI TOKEN
-    DAItoken = await ethers.getContractAt('IERC20', DAI);
-
-    // LINK TOKEN
-    LINKtoken = await ethers.getContractAt('IERC20', LINK);
-  });
-
-  it('change ETH for multiple tokens for the first account', async () => {
-    const porcents = [35 * 10, 35 * 10, 15 * 10, 15 * 10];
-    const tokens = [
-      '0x6B175474E89094C44Da98b954EedeAC495271d0F', // DAI Token
-      '0x514910771AF9Ca656af840dff83E8264EcF986CA', // LINK Token
-      '0xdAC17F958D2ee523a2206206994597C13D831ec7', // USDT Token
-      '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', // USDC Token
-    ];
-
-    await swapper.swapEthForTokens(tokens, porcents, {
-      value: ethers.utils.parseEther('5'),
-    });
-  });
-});
-
 /*
   Describing the tests, for the staking
   contract.
@@ -58,6 +16,8 @@ describe('StakingContract: Testing Staking Contract', () => {
     const StakingContract = await ethers.getContractFactory('StakingContract');
     stakingC = await upgrades.deployProxy(StakingContract, [owner.address]);
     await stakingC.deployed();
+
+    await 
   });
 
   it('should deploy the contract with the proxy', async () => {
