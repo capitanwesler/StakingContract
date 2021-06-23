ee1e762e74bc8cb9a11e6edb4797fd4d3aa17c21
capitanwesler <guillermo.campanudo@hotmail.com>
2021-06-23 14:59:36 +0100

    Adding the functions to calculete the exact amount out

--- contracts/StakingContract.sol
+++ contracts/StakingContract.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/utils/Context.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./interfaces/IWeth.sol";
 import "hardhat/console.sol";
@@ -38,42 +39,71 @@ contract StakingContract is Initializable, Context {
   }
 
   /**
+    @dev Sorts the token to see if either equal, or not.
+    @notice This is to be called in the _getReserves for the pair.
+  **/
+  function _sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
+      require(tokenA != tokenB, 'StakingContract: IDENTICAL_ADDRESSES');
+      (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+      require(token0 != address(0), 'StakingContract: ZERO_ADDRESS');
+  }
+
+  /**
+    @dev Get the reserves of the `pair`.
+    @notice The reserves of the two tokens in the pair `reserveA`, `reserveB`.
+  **/
+  function _getReserves(address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB) {
+      (address token0,) = _sortTokens(tokenA, tokenB);
+      (uint reserve0, uint reserve1,) = IUniswapV2Pair(_getAddressPair(tokenA, tokenB)).getReserves();
+      (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
+  }
+
+  /**
+    @dev Get the exact amountOut for the `amountIn` minus fees.
+  **/
+  function _getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
+      require(amountIn > 0, 'UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT');
+      require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
+      uint amountInWithFee = amountIn.mul(997);
+      uint numerator = amountInWithFee.mul(reserveOut);
+      uint denominator = reserveIn.mul(1000).add(amountInWithFee);
+      amountOut = numerator / denominator;
+  }
+
+  /**
     @dev Get the returned `amount` to exchange.
     @notice This function is for calling the `amountout` in swap.
   **/
   function _getReturn(
-    IERC20 _fromToken,
-    IERC20 _destToken,
-    address _pair,
+    address _fromToken,
+    address _destToken,
     uint256 amountIn
   ) internal view returns (uint256) {
-    uint256 reserveIn = _fromToken.balanceOf(address(_pair));
-    uint256 reserveOut = _destToken.balanceOf(address(_pair));
-
-    uint256 amountInWithFee = amountIn.mul(997);
-    uint256 numerator = amountInWithFee.mul(reserveOut);
-    uint256 denominator = reserveIn.mul(1000).add(amountInWithFee);
-    return (denominator == 0) ? 0 : numerator.div(denominator);
+    (uint256 reserveA, uint256 reserveB) = _getReserves(_fromToken, _destToken);
+    return _getAmountOut(amountIn, reserveA, reserveB);
   }
 
   /** 
     
   **/
   function getPairAndBalance(address _tokenFrom, address _tokenTo) public payable {
+    
+    /*
+      We deposit first msg.value divided by two
+      to make the swap to the specific token.
+    */
     IWeth(WETH).deposit{value: msg.value.div(2)}();
     IWeth(WETH).transfer(_getAddressPair(_tokenFrom, _tokenTo), msg.value.div(2));
     IWeth(WETH).deposit{value: msg.value.div(2)}();
 
     uint256 amount0Out = _tokenFrom == IUniswapV2Pair(_getAddressPair(_tokenFrom, _tokenTo)).token1() ? _getReturn(
-      IERC20(_tokenFrom), 
-      IERC20(_tokenTo), 
-      _getAddressPair(_tokenFrom, _tokenTo), 
+      _tokenFrom, 
+      _tokenTo, 
       msg.value.div(2)
     ) : 0;
     uint256 amount1Out = _tokenFrom == IUniswapV2Pair(_getAddressPair(_tokenFrom, _tokenTo)).token0() ? _getReturn(
-      IERC20(_tokenFrom), 
-      IERC20(_tokenTo), 
-      _getAddressPair(_tokenFrom, _tokenTo), 
+      _tokenFrom, 
+      _tokenTo, 
       msg.value.div(2)
     ) : 0;
 
