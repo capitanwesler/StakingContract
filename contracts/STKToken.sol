74f765ccc53c16482d9434a64dae953fa91b8cdd
capitanwesler <guillermo.campanudo@hotmail.com>
2021-06-23 17:32:04 +0100

    Adding the staking token

---
+++ contracts/STKToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+* @title Staking Token (STK)
+* @author Guillermo Rivas
+* @notice Implements a basic ERC20 staking token.
+*/
+contract StakingToken is ERC20, Ownable {
+   constructor() ERC20("StakingToken", "STK") {}
+}
